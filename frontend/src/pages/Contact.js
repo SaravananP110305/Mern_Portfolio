2e67c3e087f4c37ea910c0b5ea3e150854c14cd5
Saravanan P <parathk285@gmail.com>
2025-08-24 14:53:01 +0530

    Update Contact.js

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -15,7 +15,7 @@ function Contact() {
   const handleSubmit = async (e) => {
   e.preventDefault();
   try {
-    const response = await fetch('http://localhost:5000/api/contact', {
+    const response = await fetch('https://mern-portfolio-backend-gw02.onrender.com/api/contact', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData),
@@ -86,3 +86,4 @@ function Contact() {
 }
 
 export default Contact;
+
